9e20b2c3ec988ade4f7a89617da5c25baeed94b7
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-27 22:05:58 -0300

    Criando meu primeiro controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Henrick");
+        }
+
+    }
+}
